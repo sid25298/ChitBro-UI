67d0e758271d7f7b501b4052a9048a9b8876111f
Siddharth Jain <siddharthjain98@gmail.com>
2021-06-20 02:02:38 +0530

    Added Connect func

--- src/pages/App/Header/Header.js
+++ src/pages/App/Header/Header.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { CONTRACTS_SUCCESS } from '../../../redux/types';
+import { Web3Connect } from '../../../services';
 import './Header.scss'
 
 const Header = () => {
 
     const account = useSelector(state => state.contracts.account);
+    const dispatch = useDispatch();
+
+    const handleW3Connect = async () => {
+        try {
+            const [account, contract] = await Web3Connect();
+            dispatch({ type: CONTRACTS_SUCCESS, payload: { account, contract } });
+        }
+        catch (err) {
+            console.log(err)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }
 
     const handleConnect = () => {
         if (account) {
             navigator.clipboard.writeText(account);
+        }
+        else {
+            handleW3Connect();
         }
     }
 
